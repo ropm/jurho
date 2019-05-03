7b92496e64e295bc678847ec883017ba33ce3494
Roope Marttinen <roope.marttinen@hotmail.com>
2019-05-03 20:37:35 +0300

    init project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {
+    Layout,
+    Header,
+    HeaderRow,
+    Textfield,
+    Navigation,
+    Drawer,
+    Content
+} from 'react-mdl';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className='demo-big-content'>
+                <Layout>
+                    <Header waterfall>
+                        <HeaderRow title='Title'>
+                            <Textfield
+                                value=''
+                                onChange={() => {}}
+                                label='Search2'
+                                expandable
+                                expandableIcon='search'
+                            />
+                        </HeaderRow>
+                        <HeaderRow>
+                            <Navigation>
+                                <a href='#'>Link</a>
+                                <a href='#'>Link</a>
+                                <a href='#'>Link</a>
+                                <a href='#'>Link</a>
+                            </Navigation>
+                        </HeaderRow>
+                    </Header>
+                    <Drawer title='Title'>
+                        <Navigation>
+                            <a href='#'>Link</a>
+                            <a href='#'>Link</a>
+                            <a href='#'>Link</a>
+                            <a href='#'>Link</a>
+                        </Navigation>
+                    </Drawer>
+                    <Content>
+                        <div className='page-content' />
+                    </Content>
+                </Layout>
+            </div>
+        );
+    }
 }
 
 export default App;
